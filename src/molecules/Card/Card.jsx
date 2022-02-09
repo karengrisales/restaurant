79d0bcfe8d076d168ab1db4097e2cedd7fe38335
Karen Yineth Grisales Muñoz <karen.grisales@pragma.com.co>
2022-02-09 11:40:10 -0500

    Card con Flip

--- src/molecules/Card/Card.jsx
+++ src/molecules/Card/Card.jsx
@@ -6,11 +6,15 @@ import shopping from "./../../assets/img/shopping _icon.png";
 export const Card = () => {
   return (
     <div className="card">
-      <div className="card__main">
-        <img src={grey} alt="food_img" className="card__imgMain" />
-        <span className="card__title">Nombre plato</span>
+      <div className="card__container">
+        <div className="card__front">
+          <img src={grey} alt="food_img" className="card__imgMain" />
+          <span className="card__title">Nombre plato</span>
+        </div>
+        <div className="card__back">Descripción del producto</div>
       </div>
-      <div className="card__description">
+
+      <div className="card__characteristics">
         <span className="card__price">$ Precio</span>
         <img src={shopping} alt="shopping_icon" className="card__imgShopping" />
       </div>
